462e1284258e60ddaf7d992e645b439739f1e450
foolishchow <foolishchow@dingtail.com>
2020-03-22 22:07:08 +0800

    update

---
+++ reverse-proxy/reverse-proxy.js
@@ -0,0 +1,57 @@
+// write an reverse proxy with nodejs
+const net = require("net");
+const http = require("http")
+const EOL = require("os").EOL;
+const readFile = require("fs").readFileSync;
+const pathRoot = "~/Downloads/spring-boot-vue-admin-master/admin/dist";
+
+const server = http.createServer((request, response) => {
+  let path = request.url;
+  console.info(path)
+  if (/^\/(account|role|permission)/.test(path)) {
+    console.info(path)
+    let res = http.request({
+      protocol: "http:",
+      method: request.method,
+      path: request.url,
+      hostname: "localhost",
+      port: 8080,
+      headers: {
+        ...request.headers,
+        host: 'localhost:8080',
+        'x-real-ip': request.socket.remoteAddress
+      },
+      timeout: 3
+    }, (r) => {
+      for (let key in r.headers) {
+        response.setHeader(key, r.headers[key])
+      }
+      r.pipe(response)
+    });
+    request.pipe(res)
+
+  } else {
+    let res = http.request({
+      protocol: "http:",
+      method: request.method,
+      path: request.url,
+      hostname: "127.0.0.1",
+      port: 9999,
+      headers: {
+        ...request.headers,
+        host: 'localhost:9999',
+        'x-real-ip': request.socket.remoteAddress
+      },
+      timeout: 3
+    }, (r) => {
+      for (let key in r.headers) {
+        response.setHeader(key, r.headers[key])
+      }
+      r.pipe(response)
+    });
+    request.pipe(res)
+
+  }
+});
+
+server.listen(3001);
